5d6a11e0b78aa3dfb037085a78c49982f770bb2a
balpreetkg <balpreet.kaur@hotmail.com>
2019-05-10 12:20:53 -0700

    Finished Average Calculator

--- Grade Saver/AverageViewController.swift
+++ Grade Saver/AverageViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class AverageViewController: UIViewController {
     
-    @IBOutlet var collection:[UIView]!
+    var averageString = ""
+    
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        self.performSegue(withIdentifier: "AverageToMainSegue", sender: self)
+    }
+    
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    @IBOutlet var collection:[UITextField]!
     
     @IBOutlet weak var valueOfStepper: UIStepper!
-    
+
     @IBAction func submitButtonPressed(_ sender: Any) {
-        self.performSegue(withIdentifier: "GiveAverageSegue", sender: self)
+        let calculatedAverage = tryCalculation()
+        if calculatedAverage {
+            self.performSegue(withIdentifier: "GiveAverageSegue", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "GiveAverageSegue") {
+            let vc = segue.destination as! GiveAverageViewController
+            vc.average = self.averageString
+        }
     }
     
     @IBAction func stepper(_ sender: UIStepper) {
@@ -31,8 +49,42 @@ class AverageViewController: UIViewController {
         }
     }
     
+    func extractGrades(grades: [UITextField]) -> [Float] {
+        var arrayOfGrades: [Float] = []
+        for x in grades {
+            if let text = x.text, !text.isEmpty {
+                let flo = Float(text)!
+                arrayOfGrades.append(flo)
+            }
+        }
+        return arrayOfGrades
+    }
+    
+    func tryCalculation() -> Bool {
+        let grades = extractGrades(grades: collection)
+        var total: Float = 0.0
+        var amount = 0
+        for x in grades {
+            if x > 150.0 {
+                self.errorMessage.isHidden = false
+                return false
+            }
+            total = total + x
+            amount += 1
+        }
+        let average = total / Float(amount)
+        print (String(average))
+        self.averageString = String(format: "%.2f", average)
+        return true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for x in self.collection {
+            x.keyboardType = UIKeyboardType.decimalPad
+        }
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
         // ya
         // Do any additional setup after loading the view.
     }
